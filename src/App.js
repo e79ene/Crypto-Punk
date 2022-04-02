7db365acb6ce80f309607b02b8ec85d4e37d9d8c
Eugene S. Ostrovsky <eso@mail.ru>
2022-04-02 09:33:20 +0100

    Card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Header from './components/Header';
+import CollectionCard from './components/CollectionCard';
 
 function App() {
   return (
-    <div class="app">
+    <div className='app'>
       <Header />
+      <p>Hru</p>
+      <CollectionCard
+        id={0}
+        name={'bandand'}
+        traits={[{ value: 7 }]}
+        image='https://ipfs.thirdweb.com/ipfs/QmX5KmDnfBdHQhmCSnqsb4yp3dyHcY6dzd2f7L9NEgATtQ/0.jpg'
+      />
     </div>
   );
 }
